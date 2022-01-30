d2ec7a1592423ec7da2dc44ffd8c97a93b6c8935
Hee <unghee66@gmail.com>
2022-01-30 15:07:05 +0900

    [1.1]: Set up project.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+export const darkTheme = {
+  bgColor: "#000",
+  textColor: "#fff",
+  activeColor: "#ccffcc",
+};
+export const lightTheme = {
+  bgColor: "#fff",
+  textColor: "#000",
+  activeColor: "#ccffcc",
+};
